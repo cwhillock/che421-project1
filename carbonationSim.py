fd544eec7b26f4d86fe1edf4bafd48287bdc0555
Charles Whillock <charles.eka.w@gmail.com>
2024-10-06 17:11:25 -0500

    new organization structure, working virial eos

--- carbonationSim.py
+++ carbonationSim.py
@@ -1,19 +1,11 @@
 import numpy as np
 from tabulate import tabulate
-
-class Liquid:
-    def __init__(self,density):
-        self.density = density #mol/m3
-
-class Gas:
-    def __init__(self,avg_bubble_diameter):
-        self.avg_bubble_diameter = avg_bubble_diameter #m
-        self.avg_bubble_vol = 4/3*np.pi*(avg_bubble_diameter/2)**3 #average bubble volume
-        self.avg_bubble_sa = 4 * np.pi * (avg_bubble_diameter/2)**2 #average bubble surface area
+from componentPropsAndEOS import Liquid,Gas,calcZ,calcP,R
+#units of R: m3*bar/mol/K
 
 class Tank:
     def __init__(self,area,height,temp,liquid,gas,liq_holdup,gas_holdup,bubbler_flow_vol,bubbler_pressure):
-        #static geometry vars
+        #static geometry parameters
         self.area = area
         self.height = height
         self.liquid = liquid
@@ -24,23 +16,23 @@ class Tank:
         self.liq_height = self.liq_vol / area
         self.head_height = height - self.liq_height
         self.head_vol = self.head_height * area
+        self.Dab = calc_Dab(liquid,gas)
 
         #static bubbler parameters
         self.bubbler_flow_vol = bubbler_flow_vol #m3/s
-        self.bubbler_flow_mol = calc_ngas(bubbler_flow_vol,bubbler_pressure,temp)
+        self.bubbler_flow_mol = calc_ngas(self.gas,bubbler_flow_vol,bubbler_pressure,temp) #mol/s
 
         #dynamic vars
         self.gas_holdup = gas_holdup #mol
-        self.head_pressure = calc_Pgas(self.gas_holdup,self.head_vol,temp)
+        self.head_pressure = calcP(self.gas,temp,self.head_vol/gas_holdup)
         self.dissolved_gas = 0
         self.liq_conc = self.dissolved_gas / self.liq_vol
 
         #dynamic bubble vars
         self.free_gas_mol = 0 #mol
-        self.num_bubbles = self.bubbler_flow_vol / gas.avg_bubble_vol
+        self.num_bubbles = calc_vgas(self.free_gas_mol,self.head_pressure,self.temp) / gas.avg_bubble_vol
         self.total_bubble_sa = self.num_bubbles * gas.avg_bubble_sa #total surface area of bubbles
-        
-    
+
     def calc_bubble_flux(self):
         #calculate flux from bubbles to liquid
         pass
@@ -58,34 +50,26 @@ class Tank:
         #add fluxes to amount of dissolved gas
         self.dissolved_gas += flowfromhead + flowfrombubbles
         #update amount of gas in head space
-        self.gas_holdup = max(0,self.gas_holdup - flowfromhead + self.leftover_free_gas)
+        self.gas_holdup = max(0,self.gas_holdup - flowfromhead + leftover_free_gas)
         #update liquid concentration
         self.liq_conc = self.dissolved_gas / self.liq_vol
         #update head pressure
-        self.head_pressure = calc_Pgas(self.gas_holdup,self.head_vol,self.temp)
+        self.head_pressure = calcP(self.gas,self.temp,self.head_vol/self.gas_holdup)
         #update amount of free gas, bubbles, and total bubble surface area (may not be necessary)
         self.free_gas_mol = self.bubbler_flow_mol * dt
-        self.num_bubbles = self.bubbler_flow_vol / self.gas.avg_bubble_vol
+        self.num_bubbles = calc_vgas(self.free_gas_mol,self.head_pressure,self.temp) / self.gas.avg_bubble_vol
         self.total_bubble_sa = self.num_bubbles * self.gas.avg_bubble_sa
 
+def calc_Dab(liquid,gas):
+    #calculate Dab of gas in liquid
+    pass
 
-def calc_Pgas(n,v,t):
-    #calculate pressure of gas from holdup,volume, and temperature
-    #in the future should use cubic eos to calculate z
-    R = 8.31446261815324 #m3*Pa/K/mol
-    z = 1
-    return n*R*t*z/v
-
-def calc_ngas(v,p,t):
+def calc_ngas(gas,v,p,t):
     #calculate number of moles of gas from volume, pressure, and temperature
-    R = 8.31446261815324 #m3*Pa/K/mol
-    z = 1
+    z = calcZ(gas,t,p)
     return p*v/(z*R*t)
 
-def calc_vgas(n,p,t):
+def calc_vgas(gas,n,p,t):
     #calculate volume of gas from pressure and temperature
-    R = 8.31446261815324 #m3*Pa/K/mol
-    z = 1
+    z = calcZ(gas,t,p)
     return n*R*t*z/p
-
-
---
+++ componentPropsAndEOS.py
@@ -0,0 +1,36 @@
+from math import pow,pi
+R = 8.20573660809596e-5 * 1.013 #m3*bar/mol/K
+class Liquid:
+    def __init__(self,density,viscosity):
+        self.density = density #mol/m3
+        self.viscosity = viscosity #Pa*s
+
+class Gas:
+    def __init__(self,Tc,Pc,w,avg_bubble_diameter):
+        self.Tc = Tc
+        self.Pc = Pc
+        self.w = w
+        self.avg_bubble_diameter = avg_bubble_diameter #m
+        self.avg_bubble_vol = 4/3* pi *(avg_bubble_diameter/2)**3 #average bubble volume
+        self.avg_bubble_sa = 4 * pi * (avg_bubble_diameter/2)**2 #average bubble surface area
+
+def calcZ(gas,T,P):
+    Tr = T/gas.Tc
+    Pr = P/gas.Pc
+    B0 = 0.083 - 0.422/pow(Tr,1.6)
+    B1 = 0.139 - 0.172/pow(Tr,4.2)
+    Bhat = B0 + gas.w * B1
+    B = Bhat * R * gas.Tc / gas.Pc
+    Z = 1 + B * P / (R*T)
+    return Z
+
+def calcP(gas,T,V):
+    #V = SPECIFIC molar volume m3/mol
+    Tr = T/gas.Tc
+    B0 = 0.083 - 0.422/pow(Tr,1.6)
+    B1 = 0.139 - 0.172/pow(Tr,4.2)
+    Bhat = B0 + gas.w * B1
+    B = Bhat * R * gas.Tc / gas.Pc
+    return -R * T / (B-V)
+
+
